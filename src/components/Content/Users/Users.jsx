9cd8c6806ce968b1e39271676f07471997d467de
FroskyLT <kinglync1@gmail.com>
2020-10-07 23:32:24 +0300

    fixed side effect

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -4,18 +4,23 @@ import s from "./Users.module.css";
 import * as axios from "axios";
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items);
-            })
+
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items);
+                })
+        }
     }
+
 
     let singleUser = props.users.map((u) =>
         <SingleUser key={u.id} id={u.id} imgUrl={u.photos.small} name={u.name} status={u.status}
             /*country={u.country} city={u.city}*/ follow={u.followed} followToggle={props.followToggle} />)
     return (
         <div className={s.users__wrapper}>
+            <button onClick={getUsers}>Get Users</button>
             {singleUser}
         </div>
     )
