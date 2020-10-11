a692f8e60ff9e65d0cd3fc8bab116f7e9afa382e
FroskyLT <kinglync1@gmail.com>
2020-10-11 23:53:19 +0300

    changed Users function component to class component

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -14,6 +14,7 @@ class Users extends React.Component {
                 })
         }
     }
+    
     render() {
         let singleUser = this.props.users.map((u) =>
             <SingleUser key={u.id} id={u.id} imgUrl={u.photos.small} name={u.name} status={u.status}
