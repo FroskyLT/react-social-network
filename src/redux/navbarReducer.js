00e8d73730fd6149b4ca3b2ac49b8cbf41ef492b
FroskyLT <kinglync1@gmail.com>
2021-02-09 23:44:48 +0200

    rename friends in navbar

--- src/redux/navbarReducer.js
+++ src/redux/navbarReducer.js
@@ -1,8 +1,8 @@
 let initialState = {
     friendsData: [
-        { id: 1, imgUrl: "https://image.flaticon.com/icons/svg/145/145843.svg", name: "Dmitry" },
-        { id: 2, imgUrl: "https://image.flaticon.com/icons/svg/145/145843.svg", name: "Zheka" },
-        { id: 3, imgUrl: "https://image.flaticon.com/icons/svg/145/145843.svg", name: "Vladimir" }
+        { id: 1, imgUrl: "https://image.flaticon.com/icons/svg/145/145843.svg", name: "Justinas" },
+        { id: 2, imgUrl: "https://image.flaticon.com/icons/svg/145/145843.svg", name: "Roma" },
+        { id: 3, imgUrl: "https://image.flaticon.com/icons/svg/145/145843.svg", name: "Denis" }
     ]
 }
 
