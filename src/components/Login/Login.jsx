d105e133a8b422c731752d7b96adea2779516836
FroskyLT <kinglync1@gmail.com>
2021-04-02 00:44:47 +0300

    add video background to login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import CardContainer from '../common/CardContainer/CardContainer';
-import styles from './login.module.css';
+import styles from './login.module.scss';
+import video from '../../assets/videos/Samurai.mp4';
 
 const Login = (props) => {
     return (
         <div className={styles.login}>
-            <div className={styles.login__form}>
-                <CardContainer>
+            <video autoPlay className={styles.login__background}>
+                <source src={video} type="video/mp4" />
+                <source src={video} type="video/ogg" />
+                Your browser does not support the video tag.
+            </video>
+                <CardContainer className={styles.login__form}>
                     <h1>login</h1>
                     <button onClick={props.loginHandler}>sign in</button>
                 </CardContainer>
-            </div>
         </div>
     );
 }
