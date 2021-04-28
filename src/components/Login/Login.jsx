254bedf236b45afc3e92b315148bd0b84fdc9fb9
FroskyLT <kinglync1@gmail.com>
2021-04-29 00:38:46 +0300

    add video background and style textfields

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import CardContainer from '../common/CardContainer/CardContainer';
 import styles from './login.module.scss';
-import video from '../../assets/videos/Samurai.mp4';
+import video from '../../assets/videos/Samurai.webm';
 
 const Login = (props) => {
     return (
         <div className={styles.login}>
             <video autoPlay className={styles.login__background}>
-                <source src={video} type="video/mp4" />
-                <source src={video} type="video/ogg" />
+                <source src={video} type="video/webm" />
                 Your browser does not support the video tag.
             </video>
-                <CardContainer className={styles.login__form}>
-                    <h1>login</h1>
-                    <button onClick={props.loginHandler}>sign in</button>
-                </CardContainer>
+            <CardContainer className={styles.login__form}>
+                <h1>Welcome!</h1>
+                <div className={styles.form__group}>
+                    <input id="login" className={styles.form__field} type="text" placeholder="login" />
+                    <label for="login" className={styles.form__label}>login</label>
+                </div>
+                <div className={styles.form__group}>
+                    <input id="password" className={styles.form__field} type="password" placeholder="password" />
+                    <label for="password" className={styles.form__label}>password</label>
+                </div>
+                <button onClick={props.loginHandler}>sign in</button>
+            </CardContainer>
         </div>
     );
 }
