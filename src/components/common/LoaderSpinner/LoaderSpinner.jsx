ece002faa7c7c44792792dd527737cbe44e95174
FroskyLT <kinglync1@gmail.com>
2021-05-19 23:55:16 +0300

    align load spinner

--- src/components/common/LoaderSpinner/LoaderSpinner.jsx
+++ src/components/common/LoaderSpinner/LoaderSpinner.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./loader-spinner.module.scss";
 
-export const LoaderSpinner = () => <div className={styles.loader} />;
+export const LoaderSpinner = () => (
+  <div className={styles.loaderContainer}>
+    <div className={styles.loader} />
+  </div>
+);
 
 export default LoaderSpinner;
