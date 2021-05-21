89acc4097cdea5566089aae72dd95df513ad1936
FroskyLT <kinglync1@gmail.com>
2021-05-21 23:22:57 +0300

    redirect from login page when user is logged

--- src/components/Login/LoginContainer.jsx
+++ src/components/Login/LoginContainer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Redirect } from "react-router";
 
 import { authenticateMe, login } from "../../redux/reducers/authReducer";
 import Login from "./Login";
 
 class LoginContainer extends React.Component {
   render() {
+    if (this.props.isLogged) return <Redirect to="/" />;
+
     return (
       <Login
         error={this.props.error}
@@ -18,6 +21,7 @@ class LoginContainer extends React.Component {
 
 const mapStateToProps = (state) => ({
   error: state.auth.error,
+  isLogged: state.auth.isLogged,
 });
 
 export default connect(mapStateToProps, { authenticateMe, login })(
