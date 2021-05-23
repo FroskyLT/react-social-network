2dd2aa04f1cb2980b940bc7a715d7ba030a79e89
FroskyLT <kinglync1@gmail.com>
2021-05-23 19:42:30 +0300

    add app reducer

---
+++ src/redux/reducers/appReducer.js
@@ -0,0 +1,32 @@
+import { authenticateMe } from "./authReducer";
+
+const SET_INITIALIZED = "SET_INITIALIZED";
+
+let initialState = {
+  initialized: false,
+};
+
+const appReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_INITIALIZED: {
+      return {
+        ...state,
+        initialized: true,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+//AC
+export const setInitialized = () => ({ type: SET_INITIALIZED });
+
+//TC
+export const init = () => async (dispatch) => {
+  await dispatch(authenticateMe());
+
+  dispatch(setInitialized());
+};
+
+export default appReducer;
