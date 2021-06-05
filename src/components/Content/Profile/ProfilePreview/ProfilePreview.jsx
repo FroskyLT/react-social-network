2e4ed2c3179145269b0d3e61f02b8815490755b5
FroskyLT <kinglync1@gmail.com>
2021-06-05 17:57:29 +0300

    convert class component function component and add text selection styles

--- src/components/Content/Profile/ProfilePreview/ProfilePreview.jsx
+++ src/components/Content/Profile/ProfilePreview/ProfilePreview.jsx
@@ -2,68 +2,65 @@ import React from "react";
 import styles from "./profile-preview.module.scss";
 import Button from "../../../common/Button/Button";
 
-class ProfilePreview extends React.Component {
-  render() {
-    const profileId = this.props.profileData.userId;
-    const isFollowInProgress = this.props.followInProgress.some(
-      (userId) => userId === profileId
+const ProfilePreview = (props) => {
+  const profileId = props.profileData.userId;
+  const isFollowInProgress = props.followInProgress.some(
+    (userId) => userId === profileId
+  );
+  const currUserProfile = props.currUserId === profileId;
+  const isFollowed = props.isFollowingUser;
+
+  if (props.currUserId && !currUserProfile) props.isFollowing(profileId);
+
+  const followButton = () => {
+    return isFollowed ? (
+      <Button
+        clickHandler={() => props.onUnfollow(profileId)}
+        disabled={isFollowInProgress}
+      >
+        {"unfollow"}
+      </Button>
+    ) : (
+      <Button
+        clickHandler={() => props.onFollow(profileId)}
+        disabled={isFollowInProgress}
+      >
+        {"follow"}
+      </Button>
     );
-    const currUserProfile = this.props.currUserId === profileId;
-    const isFollowed = this.props.isFollowingUser;
+  };
 
-    if (this.props.currUserId && !currUserProfile)
-      this.props.isFollowing(profileId);
-
-    const followButton = () => {
-      return isFollowed ? (
-        <Button
-          clickHandler={() => this.props.onUnfollow(profileId)}
-          disabled={isFollowInProgress}
-        >
-          {"unfollow"}
-        </Button>
-      ) : (
-        <Button
-          clickHandler={() => this.props.onFollow(profileId)}
-          disabled={isFollowInProgress}
-        >
-          {"follow"}
-        </Button>
-      );
-    };
-
-    return (
-      <div className={styles.profilePreview}>
-        <div className={styles.wallpaper}>
-          <img
-            src={
-              "https://images.hdqwalls.com/wallpapers/red-lake-mountains-minimal-4k-1u.jpg"
-            }
-            alt=""
-          />
-        </div>
-        <div className={styles.about}>
-          <img
-            src={
-              this.props.profileData.photos.large ||
-              "https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/84-512.png"
-            }
-            alt=""
-          />
-          <div className={styles.about__description}>
-            <div className={styles.about__name}>
-              {this.props.profileData.fullName ?? "No Name"}
-            </div>
-            {!this.props.currUserId || currUserProfile ? (
-              <div className={styles.about__followPlaceholder} />
-            ) : (
-              followButton()
-            )}
+  return (
+    <div className={styles.profilePreview}>
+      <div className={styles.wallpaper}>
+        <img
+          src={
+            "https://images.hdqwalls.com/wallpapers/red-lake-mountains-minimal-4k-1u.jpg"
+          }
+          alt=""
+        />
+      </div>
+      <div className={styles.about}>
+        <img
+          src={
+            props.profileData.photos.large ||
+            "https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/84-512.png"
+          }
+          alt=""
+        />
+        <div className={styles.about__description}>
+          <div className={styles.about__name}>
+            {props.profileData.fullName ?? "No Name"}
           </div>
+          {!props.currUserId || currUserProfile ? (
+            <div className={styles.about__followPlaceholder} />
+          ) : (
+            followButton()
+          )}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ProfilePreview;
