a450a6bb19a4354638de3de602e2d3d71a5e6105
FroskyLT <kinglync1@gmail.com>
2021-06-07 23:14:21 +0300

    add redux devtools config

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import profileReducer from "./reducers/profileReducer";
 import messagesReducer from "./reducers/messagesReducer";
 import usersReducer from "./reducers/usersReducer";
@@ -14,7 +14,12 @@ const reducers = combineReducers({
   app: appReducer,
 });
 
-const store = createStore(reducers, applyMiddleware(ThunkMiddleware));
+// Redux DevTools in Chrome
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(ThunkMiddleware))
+);
 
 window.store = store;
 
