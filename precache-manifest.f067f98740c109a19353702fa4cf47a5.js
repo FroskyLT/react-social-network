328e562d2fc6dbb90c545361c8d5badcf6436df4
FroskyLT <kinglync1@gmail.com>
2021-06-18 18:17:41 +0300

    Updates

---
+++ precache-manifest.f067f98740c109a19353702fa4cf47a5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842d3aaedb4d2598f23893feba350a36",
+    "url": "/react-social-network/index.html"
+  },
+  {
+    "revision": "06f55df7bd315f2f42ed",
+    "url": "/react-social-network/static/css/4.a122dbbb.chunk.css"
+  },
+  {
+    "revision": "e24593abddf9524644c4",
+    "url": "/react-social-network/static/css/5.097f5056.chunk.css"
+  },
+  {
+    "revision": "ab653871e675c0639d08",
+    "url": "/react-social-network/static/css/6.07626caf.chunk.css"
+  },
+  {
+    "revision": "9388b24b6fa31c9455a9",
+    "url": "/react-social-network/static/css/7.e68fe17e.chunk.css"
+  },
+  {
+    "revision": "214b55af1ab98cadde01",
+    "url": "/react-social-network/static/css/main.ea681799.chunk.css"
+  },
+  {
+    "revision": "a205492c02ba22a5ea38",
+    "url": "/react-social-network/static/js/0.3b158e24.chunk.js"
+  },
+  {
+    "revision": "fb81ee245b9593c099d3",
+    "url": "/react-social-network/static/js/3.55875749.chunk.js"
+  },
+  {
+    "revision": "06f55df7bd315f2f42ed",
+    "url": "/react-social-network/static/js/4.68979254.chunk.js"
+  },
+  {
+    "revision": "e24593abddf9524644c4",
+    "url": "/react-social-network/static/js/5.f63ef4e6.chunk.js"
+  },
+  {
+    "revision": "ab653871e675c0639d08",
+    "url": "/react-social-network/static/js/6.1c25a35d.chunk.js"
+  },
+  {
+    "revision": "9388b24b6fa31c9455a9",
+    "url": "/react-social-network/static/js/7.b9948ae1.chunk.js"
+  },
+  {
+    "revision": "73e9f0373f098e692774",
+    "url": "/react-social-network/static/js/8.1b2a40f5.chunk.js"
+  },
+  {
+    "revision": "214b55af1ab98cadde01",
+    "url": "/react-social-network/static/js/main.e31cab6c.chunk.js"
+  },
+  {
+    "revision": "0891b18b86b9f6ccff37",
+    "url": "/react-social-network/static/js/runtime-main.bcdbad98.js"
+  },
+  {
+    "revision": "10444ca9ec130875343804d1f1658c22",
+    "url": "/react-social-network/static/media/Samurai.10444ca9.webm"
+  }
+]);
