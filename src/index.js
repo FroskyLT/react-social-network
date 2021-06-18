d6d81ad8a942b563fb9079242137b9961cc5e89b
FroskyLT <kinglync1@gmail.com>
2021-06-18 22:53:58 +0300

    deploy app on github pages

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import "./styles/index.scss";
 import App from "./App";
 import store from "./redux/reduxStore";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
