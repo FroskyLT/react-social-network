826040c8cdccb8918203f72cd25c56c015785e5f
FroskyLT <kinglync1@gmail.com>
2021-06-29 00:22:34 +0300

    create edit mode modal demo

--- src/components/Content/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Content/Profile/ProfileInfo/ProfileInfo.jsx
@@ -15,6 +15,7 @@ import {
   FaEdit,
 } from "react-icons/fa";
 import Modal from "../../../common/Modal/Modal";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const SectionTitle = ({ children }) => (
   <h2 className={styles.sectionTitle}>{children}</h2>
@@ -172,7 +173,7 @@ const Friends = ({ friends, currUserPage }) => {
   );
 };
 
-const EditMode = ({ currUserPage }) => {
+const EditMode = ({ currUserPage, profile }) => {
   const [showModal, setShowModal] = useState(false);
   return (
     <>
@@ -183,11 +184,123 @@ const EditMode = ({ currUserPage }) => {
         />
       )}
       <Modal
-        showModal={showModal}
+        // showModal={showModal}
+        showModal={true}
         title="Edit Mode"
         closeModal={() => setShowModal(false)}
       >
-        {"test"}
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values, { setSubmitting }) => {
+            // props.loginHandler(values);
+            setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting, values }) => (
+            <Form>
+              <div className={styles.editMode__contacts}>
+                <FaFacebook
+                  className={`${styles.editMode__contact} ${styles.editMode__contact_selected}`}
+                />
+                <FaGithub className={styles.editMode__contact} />
+                <FaInstagram className={styles.editMode__contact} />
+                <FaLink className={styles.editMode__contact} />
+                <FaTwitter className={styles.editMode__contact} />
+                <FaVk className={styles.editMode__contact} />
+                <FaGlobeAmericas className={styles.editMode__contact} />
+                <FaYoutube className={styles.editMode__contact} />
+              </div>
+              <div className={styles.form__group}>
+                <label
+                  htmlFor="fullName"
+                  className={
+                    values.email
+                      ? styles.form__label
+                      : `${styles.form__label} ${styles.form__label_big}`
+                  }
+                >
+                  Full Name
+                </label>
+                <Field
+                  id="fullName"
+                  type="text"
+                  name="fullName"
+                  className={styles.form__field}
+                  required
+                />
+                <div className={styles.form__error}>
+                  <ErrorMessage
+                    name="fullName"
+                    component="div"
+                    className={styles.form__error}
+                  />
+                </div>
+              </div>
+              <div className={styles.form__group}>
+                <label
+                  htmlFor="lookingForAJob"
+                  className={
+                    values.password
+                      ? styles.form__label
+                      : `${styles.form__label} ${styles.form__label_big}`
+                  }
+                >
+                  Are you open to new opportunities?
+                </label>
+                <Field
+                  id="lookingForAJob"
+                  type="checkbox"
+                  name="lookingForAJob"
+                  className={styles.form__field}
+                  required
+                />
+                <div className={styles.form__error}>
+                  <ErrorMessage
+                    name="lookingForAJob"
+                    component="div"
+                    className={styles.form__error}
+                  />
+                </div>
+              </div>
+              <div className={styles.form__group}>
+                <label
+                  htmlFor="lookingForAJobDescription"
+                  className={
+                    values.email
+                      ? styles.form__label
+                      : `${styles.form__label} ${styles.form__label_big}`
+                  }
+                >
+                  Hard Skills
+                </label>
+                <Field
+                  id="lookingForAJobDescription"
+                  type="text"
+                  name="lookingForAJobDescription"
+                  className={styles.form__field}
+                  required
+                />
+                <div className={styles.form__error}>
+                  <ErrorMessage
+                    name="lookingForAJobDescription"
+                    component="div"
+                    className={styles.form__error}
+                  />
+                </div>
+              </div>
+              {/* <div className={styles.form__error}>
+                {props.error && props.error}
+              </div> */}
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className={styles.form__submit}
+              >
+                Edit
+              </button>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     </>
   );
@@ -201,7 +314,7 @@ const ProfileInfo = (props) => {
   return (
     <>
       <CardContainer>
-        <EditMode currUserPage={currUserPage} />
+        <EditMode currUserPage={currUserPage} profile={profile} />
         <SectionTitle>{"Intro"}</SectionTitle>
         <SectionStatus
           status={status}
